d140c3c46bb4439c37544b998ccf882ed6d31c85
Aira Arima <arimacoelhoaira@gmail.com>
2024-01-23 12:33:36 -0300

    criação atributos entidade project

--- backend/src/main/java/com/orange/backend/model/Project.java
+++ backend/src/main/java/com/orange/backend/model/Project.java
@@ -1,4 +1,26 @@
 package com.orange.backend.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    private String tags;
+
+    private String links;
+
+    @Column(nullable = false)
+    private byte[] imagem;
 }
